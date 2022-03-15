b245d3d57f890d2ba950981a10fd5cb18628e88e
sbr-vincent <vincent.g.sbr@gmail.com>
2022-03-15 13:46:53 -0700

    Added Login/Register and Goals

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import {useState, useEffect} from 'react'
 import { FaSignInAlt } from 'react-icons/fa'
+import {useSelector, useDispatch} from 'react-redux'
+import {useNavigate} from 'react-router-dom'
+import {toast} from 'react-toastify'
+import {login, reset} from '../features/auth/authSlice'
+import Spinner from '../components/Spinner'
 
 function Login() {
     const [formData, setFormData] = useState({
@@ -9,6 +14,25 @@ function Login() {
     })
 
     const {email, password} = formData
+
+    const navigate = useNavigate()
+    const dispatch = useDispatch()
+
+    const {user, isLoading, isError, isSuccess, message} = useSelector((state) => state.auth)
+
+    useEffect(() => {
+        if(isError){
+            console.log("bnana")
+            toast.error(message)
+        }
+
+        if(isSuccess || user){
+            console.log("taco")
+            navigate('/')
+        }
+
+        dispatch(reset())
+    },[user, isError, isSuccess, message, navigate, dispatch])
 
     const onChange = (e) => {
         setFormData((prevState) => ({
@@ -19,6 +43,17 @@ function Login() {
 
     const onSubmit = (e) => {
         e.preventDefault()
+
+        const userData = {
+            email,
+            password,
+        }
+
+        dispatch(login(userData))
+    }
+
+    if(isLoading) {
+        return <Spinner />
     }
 
     return (
@@ -54,10 +89,10 @@ function Login() {
                             onChange={onChange} 
                         />
                     </div>
+                    <div className="form-group">
+                        <button type="submit" className='btn btn-block'>Submit</button>
+                    </div>
                 </form>  
-                <div className="form-group">
-                    <button type="submit" className='btn btn-block'>Submit</button>
-                </div>
             </section>
         </>
     )
